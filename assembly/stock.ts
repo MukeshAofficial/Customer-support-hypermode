83bb724f94ea4c71da34a7e3905552bc79ee90e1
Mukesh <mukeshofficial85@gmail.com>
2025-01-01 13:39:52 +0530

    Created Frontend

--- assembly/stock.ts
+++ assembly/stock.ts
@@ -14,3 +14,31 @@ export function callStockPriceApi(symbol: string): string {
 
   return responseText;
 }
+export function fetchNews(symbol: string): string {
+  const url = `https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=${symbol}&apikey={{MODUS_API_KEY}}`;
+  const req = new http.Request(url);
+  const resp = http.fetch(req);
+
+  if (!resp.ok) {
+    throw new Error(`Failed to fetch news data. Received: ${resp.status} ${resp.statusText}`);
+  }
+
+  const responseText = resp.text();
+  console.log(responseText);
+
+  return responseText;
+}
+export function fetchCompanyinfo(symbol: string): string {
+  const url = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${symbol}&apikey={{MODUS_API_KEY}}`;
+  const req = new http.Request(url);
+  const resp = http.fetch(req);
+
+  if (!resp.ok) {
+    throw new Error(`Failed to fetch news data. Received: ${resp.status} ${resp.statusText}`);
+  }
+
+  const responseText = resp.text();
+  console.log(responseText);
+
+  return responseText;
+}
